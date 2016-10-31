9e971469b4f1557fb8564da68cdf42af8b0ed7f8
Von Random <von@vdrandom.org>
2016-10-31 15:20:07 +0300

    load average as well

--- plugins/load.py
+++ plugins/load.py
@@ -13,9 +13,16 @@ class PluginThread(threading.Thread):
             self.status['color'] = config.get(section, 'color')
         self.freq = config.getint(section, 'freq', fallback=10)
         self.hide = False
+        self.problem_value = config.getint(section, 'problem', fallback=100)
 
     def main(self):
         loads = os.getloadavg()
+        if loads[0] >= self.problem_value:
+            self.hide = False
+            self.status['urgent'] = True
+        else:
+            self.hide = True
+            self.status['urgent'] = False
         loads = [str(i) for i in loads]
         self.status['full_text'] = 'LA: ' + ' '.join(loads)
 
