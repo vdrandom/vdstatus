cc402b0e23b9b78c4ea47f7a25638219e2a1aced
Von Random <von@vdrandom.org>
2016-10-31 15:10:30 +0300

    disk check to auto hide as well

--- plugins/disk.py
+++ plugins/disk.py
@@ -13,10 +13,16 @@ class PluginThread(threading.Thread):
             self.status['color'] = config.get(section, 'color')
         self.freq = config.getint(section, 'freq', fallback=30)
         self.hide = False
+        self.problem_value = config.getint(section, 'problem', fallback=70)
 
     def main(self):
         du_stat = psutil.disk_usage(self.part)
-        # du_perc = str(du_stat.percent)
+        if du_stat.percent >= self.problem_value:
+            self.hide = False
+            self.status['urgent'] = True
+        else:
+            self.hide = True
+            self.status['urgent'] = False
         du_free = str(round(du_stat.free / 2**30, 2))
         du = self.part + ': ' + du_free + 'G'
         self.status['full_text'] = du
