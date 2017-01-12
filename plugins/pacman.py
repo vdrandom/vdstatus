c39132ab599cd0694618a3563371f645287dc112
Von Random <von@vdrandom.org>
2017-01-12 16:56:32 +0300

    add pacman plugin to monitor the number of available updates

---
+++ plugins/pacman.py
@@ -0,0 +1,22 @@
+import os
+import random
+import plugins
+import subprocess
+
+
+class PluginThread(plugins.PluginThreadCommon):
+    def __init__(self, section, config):
+        super(PluginThread, self).__init__(section, config)
+        self.freq = config.getint(section, 'freq', fallback=180)
+        self.format_status(0)
+
+    def format_status(self, count):
+        self.status['full_text'] = 'UPD: ' + str(count)
+        if count > 0:
+            self.hide = False
+        else:
+            self.hide = True
+
+    def main(self):
+        updates = subprocess.getoutput('/usr/bin/pacman -Sup').count("\n")
+        self.format_status(updates)
