f5c49008ec9770d64b6f7217098207116c2092d5
Von Random <von@vdrandom.org>
2017-11-30 01:20:35 +0300

    add pytz to the mix, so that we can display time for specific time zones (totally optional dependency)

--- plugins/date.py
+++ plugins/date.py
@@ -1,4 +1,4 @@
-import time
+import datetime
 import plugins
 
 
@@ -6,6 +6,13 @@ class PluginThread(plugins.PluginThreadCommon):
     def __init__(self, section, config):
         super(PluginThread, self).__init__(section, config)
         self.date_format = config.get(section, 'format', fallback='%c')
+        tz = config.get(section, 'TZ', fallback=None)
+        if tz:
+            import pytz
+            self.tz = pytz.timezone(tz)
+        else:
+            self.tz = None
 
     def main(self):
-        self.status['full_text'] = time.strftime(self.date_format)
+        now = datetime.datetime.now(tz=self.tz)
+        self.status['full_text'] = now.strftime(self.date_format)
