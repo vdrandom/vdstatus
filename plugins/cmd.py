8e1cea8a01417b51a9dea4bea888ced21aadd251
Von Random <von@vdrandom.org>
2020-03-24 17:50:45 +0300

    conf: some comment clarification, get rid of removed plugin; cmd: fix variable name leftovers from when it was just pacman plugin

--- plugins/cmd.py
+++ plugins/cmd.py
@@ -13,11 +13,11 @@ class PluginThread(plugins.PluginThreadCommon):
         super(PluginThread, self).__init__(config, PACMAN_DEFAULTS)
 
     def main(self):
-        pacman_qu = subprocess.Popen(
+        proc = subprocess.Popen(
             self.conf['cmd'], stdout=subprocess.PIPE,
             stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL,
             encoding='UTF-8'
         )
-        out = pacman_qu.communicate()[0].strip().splitlines()[0]
+        out = proc.communicate()[0].strip().splitlines()[0]
 
         self.format_status(out)
